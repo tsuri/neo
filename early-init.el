a2ad2222f262f354b74ded008d63a908987bb47e
Maurizio Vitale <mav@polymath-solutions.com>
2021-09-01 21:57:24 -0400

    Reasonable defaults

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . 0) default-frame-alist)
